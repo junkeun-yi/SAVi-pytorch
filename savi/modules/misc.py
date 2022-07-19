584d3bde03832ac413d3c2b649c90aebbe7b531f
Junkeun Yi <junkeun.yi@berkeley.edu>
2022-07-19 00:30:07 +0000

    cahnge back gru init

--- savi/modules/misc.py
+++ savi/modules/misc.py
@@ -179,15 +179,10 @@ class myGRUCell(nn.Module):
 		if self.weight_init is not None:
 			weight_init = init_fn[self.weight_init['linear_w']]
 			bias_init = init_fn[self.weight_init['linear_b']]
-		elif self.weight_init is None:
-			self.weight_init = {'linear_w': 'default', 'linear_b': 'default'}
-		if self.weight_init['linear_w'] == 'default':
+		else:
+			# weight init not given
 			stdv = 1.0 / math.sqrt(self.hidden_size) if self.hidden_size > 0 else 0
-			weight_init = lambda weight: nn.init.uniform_(weight, -stdv, stdv)
-		if self.weight_init['linear_b'] == 'default':
-			stdv = 1.0 / math.sqrt(self.hidden_size) if self.hidden_size > 0 else 0
-			bias_init = lambda weight: nn.init.uniform_(weight, -stdv, stdv)
-		# used to just init them as xavier normal and zeros but changed to default gru init
+			weight_init = bias_init = lambda weight: nn.init.uniform_(weight, -stdv, stdv)
 		# input weights
 		weight_init(self.dense_ir.weight)
 		bias_init(self.dense_ir.bias)
