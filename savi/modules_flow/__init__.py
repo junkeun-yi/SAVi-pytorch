6f9a1c8987995aa7729bb4cbcf3a027a88dadf94
Junkeun Yi <junkeun.yi@berkeley.edu>
2022-08-10 22:49:18 +0000

    remove unnecessary files

--- savi/modules_flow/__init__.py
+++
@@ -1,6 +0,0 @@
-"""Init"""
-
-from .model import (FramePrediction, FlowPrediction, FlowWarp)
-from .decoders import (SpatialBroadcastMaskDecoder)
-from .misc import (L2Loss, ARI)
-from .factory import build_modules as flow_build_modules
